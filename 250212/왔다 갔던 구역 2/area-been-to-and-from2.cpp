d2d817da089b63cadd5b8baeae3ceff2bd1666d7
hm5479 <164597360+hm5479@users.noreply.github.com>
2025-02-12 18:03:42 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250212/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define OFFSET 1000
+
+vector<int> vec(2000, 0);
+
+void move(int curr, int x) {
+    if (x >= 0)
+        for (int i = curr; i < curr + x; i++)
+            vec[i]++;
+    else
+        for (int i = curr; i > curr + x; i--)
+            vec[i]++;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    int curr = OFFSET;
+    while (N--) {
+        int x;
+        char dir;
+        cin >> x >> dir;
+
+        if (dir == 'L')
+            x *= -1;
+
+        move(curr, x);
+        curr += x;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] > 1)
+            ans++;
+    }
+
+    cout << ans;
+}
