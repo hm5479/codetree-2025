b481c4471715b3d3726b22408a4bca2e587f7fdf
hm5479 <164597360+hm5479@users.noreply.github.com>
2025-02-19 16:21:07 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250219/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> vec(N);
+    for (int i = 0; i < N; i++)
+        cin >> vec[i];
+
+    int ans = 0, cnt = 0;
+    for (int i = 0; i < N; i++) {
+        if (i > 0 && vec[i] * vec[i - 1] > 0)
+            cnt++;
+        else
+            cnt = 1;
+        ans = max(cnt, ans);
+    }
+
+    cout << ans;
+}
